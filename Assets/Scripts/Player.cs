39bb9d544fe4ee7b8198f82f96e771ca5a062a6a
Alfonso David Navarro <32369156+adnavarro@users.noreply.github.com>
2021-05-25 12:21:37 -0400

    Player Movement
    Now we can move the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontalInput, verticalInput) * _speed * Time.deltaTime);
     }
 }
