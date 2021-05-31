29e2f7ac6422277ce8ec77d85a7e2aad13271a57
Alfonso David Navarro <32369156+adnavarro@users.noreply.github.com>
2021-05-31 17:08:38 -0400

    Enemy
    Now we can collide with the enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -18,10 +18,25 @@ public class Enemy : MonoBehaviour
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
-        if (transform.position.y <= -5.4f)
+        if (transform.position.y < -5f)
         {
-            float randomX = Random.Range(-9.5f, 9.5f);
+            float randomX = Random.Range(-8f, 8f);
             transform.position = new Vector3(randomX, 7.4f, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit by: " + other.transform.name);
+
+        if (other.transform.name == "Player")
+        {
+            Destroy(GameObject.FindWithTag(other.transform.name));
+        }
+        else if (other.transform.name == "Laser(Clone)")
+        {
+            Destroy(GameObject.FindWithTag("Laser"));
+            Destroy(this.gameObject);
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,7 +15,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 0, 0);
+        transform.position = new Vector3(0, -2, 0);
     }
 
     // Update is called once per frame
