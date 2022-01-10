ccad7e9958e93226e8b36b2a9d655d685ab2b3bd
Alfonso David Navarro <32369156+adnavarro@users.noreply.github.com>
2022-01-10 12:50:29 -0400

    Separated player actions

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Player_0 PlayerScript;
+    private float _speed = 1.5f;
+    private Vector3 moveDelta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerScript.GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        CalculateMovement();
+    }
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        moveDelta = new Vector3(horizontalInput, verticalInput);
+
+        if (moveDelta.x > 0)
+        {
+            PlayerScript.transform.localScale = Vector3.one;
+        }
+        else if (moveDelta.x < 0)
+        {
+            PlayerScript.transform.localScale = new Vector3(-1, 1, 0);
+        }
+
+        PlayerScript.Move(moveDelta, _speed);
+    }
+}
+
+
--- Assets/Scripts/Player_0.cs
+++ Assets/Scripts/Player_0.cs
@@ -4,38 +4,21 @@ using UnityEngine;
 
 public class Player_0 : MonoBehaviour
 {
-    [SerializeField]
-    private float _speed = 1.5f;
-    private Vector3 moveDelta;
     private Rigidbody2D rigidBody;
-    // Start is called before the first frame update
-    void Start()
+    
+
+    void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         rigidBody.freezeRotation = true;
     }
-
-    void FixedUpdate()
+    // Start is called before the first frame update
+    void Start()
     {
-        CalculateMovement();
+
     }
-
-    void CalculateMovement()
+    public void Move(Vector3 moveDelta, float speed)
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        moveDelta = new Vector3(horizontalInput, verticalInput);
-
-        if (moveDelta.x > 0)
-        {
-            transform.localScale = Vector3.one;
-        }
-        else if (moveDelta.x < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 0);
-        }
-
-        rigidBody.velocity = (moveDelta * _speed);
+        rigidBody.velocity = (moveDelta * speed);
     }
 }
